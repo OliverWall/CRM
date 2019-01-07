5f992a21deb4f65ce9e9e17d665a4a1d650792dc
RoryRou <33131980+RoryRou@users.noreply.github.com>
2019-01-07 12:31:26 +0300

    created method login of user

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://crm:Qq1234567@ds026658.mlab.com:26658/crm'
+    mongoURI: 'mongodb://crm:Qq1234567@ds026658.mlab.com:26658/crm',
+    jwt: 'dev-jwt'
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,29 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        email: req.body.email,
-        password: req.body.password,
-    })
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id,
+            }, keys.jwt, {expiresIn: 3600});
+
+            res.status(200).json({token: `Bearer ${token}`});
+        } else {
+            res.status(401).json({message: "Указан неверный пароль"});
+        }
+
+    } else {
+        res.status(404).json({message: "Пользователь не найден"});
+    }
 };
 
 module.exports.register = async function(req, res) {
