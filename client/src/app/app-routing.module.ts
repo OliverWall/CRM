774a12dd52e6cf641d1f4ef00ed0a3653e76f51c
RoryRou <33131980+RoryRou@users.noreply.github.com>
2019-05-12 14:10:45 +0300

    initial setup of authorization component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+import {AuthComponent} from './common/layout/auth/auth.component';
+import {SiteComponent} from './common/layout/site/site.component';
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
+
+
+const routes: Routes = [
+  {
+    path: 'auth', component: AuthComponent, children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'reg', component: RegistrationPageComponent}
+    ]
+  },
+  {
+    path: 'site', component: SiteComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { AuthComponent } from './common/layout/auth/auth.component';
+import { SiteComponent } from './common/layout/site/site.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SiteComponent,
+    LoginPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
